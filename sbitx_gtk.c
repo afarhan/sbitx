afarhan/sbitx

commit 04277ad2e119b3cbc63105dcdb72d329efddbbba

implemented selection field roll over

sbitx_gtk.c
@@ -1692,7 +1692,15 @@ static void edit_field(struct field *f, int action){
 		sprintf(f->value, "%d",  v);
 	}
 	else if (f->value_type == FIELD_SELECTION){
-		char *p, *prev, *next, b[100];
+		char *p, *prev, *next, b[100], *first, *last; // k3ng 2022-08-23 implemented field selection roll over
+    // get the first and last selections
+    strcpy(b, f->selection);
+    p = strtok(b, "/");
+    first = p;
+    while(p){
+      last = p;
+      p = strtok(NULL, "/");
+    }
 		//search the current text in the selection
 		prev = NULL;
 		strcpy(b, f->selection);
@@ -1715,6 +1723,7 @@ static void edit_field(struct field *f, int action){
 			if (p)
 				strcpy(f->value, p);
 			else
+        strcpy(f->value, first); // roll over
 				return;
 				//strcpy(f->value, prev); 
 		}
@@ -1722,6 +1731,7 @@ static void edit_field(struct field *f, int action){
 			if (prev)
 				strcpy(f->value, prev);
 			else
+        strcpy(f->value, last); // roll over
 				return;
 		}
 	}

sdr_ui.h
@@ -25,4 +25,4 @@ void update_log_ed();
 void write_call_log();
 time_t time_sbitx();
 
-#define VER_STR "sbitx v1.1.k3ng.20220823.1500"
+#define VER_STR "sbitx v1.1.k3ng.20220823.0122"

commit 07a5d5faf767411629c93819a584d7139ad1c2ce

fixed issue with FIELD_SELECTION buttons not updating text caused by commit 04277ad

sbitx_gtk.c
@@ -1724,7 +1724,7 @@ static void edit_field(struct field *f, int action){
 				strcpy(f->value, p);
 			else
         strcpy(f->value, first); // roll over
-				return;
+				//return;
 				//strcpy(f->value, prev); 
 		}
 		else if (action == MIN_KEY_UP){
@@ -1732,7 +1732,7 @@ static void edit_field(struct field *f, int action){
 				strcpy(f->value, prev);
 			else
         strcpy(f->value, last); // roll over
-				return;
+				//return;
 		}
 	}
 	else if (f->value_type == FIELD_TOGGLE){
@@ -2912,6 +2912,7 @@ void hw_init(){
 	enc_init(&enc_b, ENC_FAST, ENC2_A, ENC2_B);
 
 	int e = g_timeout_add(1, ui_tick, NULL); // k3ng 2022-08-16
+	//int e = g_timeout_add(10, ui_tick, NULL); // original
 
 	wiringPiISR(ENC2_A, INT_EDGE_BOTH, tuning_isr);
 	wiringPiISR(ENC2_B, INT_EDGE_BOTH, tuning_isr);
@@ -2992,6 +2993,7 @@ gboolean ui_tick(gpointer gook){
 	}
 
 	if (ticks == 100){ // k3ng 2022-08-16
+	//if (ticks == 10){ // original 
 
 		struct field *f = get_field("spectrum");
 		update_field(f);	//move this each time the spectrum watefall index is moved

sdr_ui.h
@@ -25,4 +25,4 @@ void update_log_ed();
 void write_call_log();
 time_t time_sbitx();
 
-#define VER_STR "sbitx v1.1.k3ng.20220823.0122"
+#define VER_STR "sbitx v1.1.k3ng.20220824.1522"